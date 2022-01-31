f80b1611f684c79b4442e0b98d4ee5885c8e095f
sridhar11-dev <52885573+sridhar11-dev@users.noreply.github.com>
2022-01-31 23:20:51 +0530

    Add files via upload
    week2 day1 classroom and Practice Assignments

---
+++ week2/day1/TestCase2.java
@@ -0,0 +1,26 @@
+package week2.day1;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class TestCase2 {
+	public static void main(String[] args) {
+		WebDriverManager.chromedriver().setup();
+		ChromeDriver driver = new ChromeDriver();
+		driver.get("http://leaftaps.com/opentaps/control/main");
+		driver.manage().window().maximize();
+		driver.get("http://leaftaps.com/opentaps/control/main");
+		driver.findElement(By.id("username")).sendKeys("Demosalesmanager");
+		driver.findElement(By.id("password")).sendKeys("crmsfa");
+		driver.findElement(By.className("decorativeSubmit")).click();
+		driver.findElement(By.partialLinkText("CRM/SFA")).click();
+		driver.findElement(By.partialLinkText("Contacts")).click();
+		driver.findElement(By.partialLinkText("Find Contacts")).click();
+		driver.findElement(By.xpath("//span[text()='Email']")).click();
+		driver.findElement(By.name("emailAddress")).sendKeys("babu@testleaf.com");
+		driver.findElement(By.xpath("//button[text()='Find Contacts']")).click();
+		driver.close();
+	}
+}
